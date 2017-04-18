af19445e576be53b8d95a56ddb1d77a1b311627f
Unknown <geethmadushankagamage@gmail.com>
2017-04-18 09:35:39 -0700

    View only
    Home Page

---
+++ Views/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Yaathra</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Yaathra</a>
+    </div>
+
+     <form class="navbar-form navbar-left">
+      <div class="input-group" >
+        <input type="text" class="form-control" size="35" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+    <div class="tab">
+    <ul class="nav navbar-nav navbar-right"> 
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About us</a></li>
+      <li><a href="#">Blog</a></li>  
+      <li><a href="#">Contact</a></li>  
+
+    </ul>
+    </div>
+       <div class="btnlogin"><button type="button" class="btn">Login or Register</button></div>
+  
+  </div>
+</nav>
+
+
+
+<div class="col-md-12 left-sidebar">
+ <button type="button" class="btn btn-success btn-md">
+      <span class="glyphicon glyphicon-plus"></span> ASK A QUESTION
+    </button>
+
+</div>
+
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- create side bar manu -->
+<div class="container">
+    <div class="row">
+        <div class="col-xs-2 bordered">
+            <ul class="nav nav-pills nav-stacked">
+                
+                <li><a href="#"><i class="glyphicon glyphicon-question-sign"></i>  Questions</a></li>
+                <li><a href="#"><i class="glyphicon glyphicon-tags"></i>  Tags</a></li>
+                <li><a href="#"><i class="fa fa-trophy"></i>  Badges</a></li>
+                <li><a href="#"><i class="fa fa-th-list"></i>  Categories</a></li>
+                <li><a href="#"><i class="fa fa-users"></i>  Users</a></li>
+            </ul>
+        </div>
+        
+    </div>
+</div>
+
+
+
+
+  <!-- <h3>Navbar Forms</h3>
+  <p>Use the .navbar-form class to vertically align form elements (same padding as links) inside the navbar.</p>
+  <p>The .input-group class is a container to enhance an input by adding an icon, text or a button in front or behind it as a "help text".</p>
+  <p>The .input-group-btn class attaches a button next to an input field. This is often used as a search bar:</p> -->
+
+
+</body>
+</html>
